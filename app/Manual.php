aba2488e984329345594fe1255a5ae915c0bc8b5
Dominik Nowak <domin1204@gmail.com>
2017-04-20 09:49:52 +0200

    Manual method

--- app/Manual.php
+++ app/Manual.php
@@ -7,6 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Manual extends Model
 {
     protected $fillable = [
-        'name'
+        'name', 'link_rewrite'
     ];
+
+    public function link($absolute = false){
+        $link = '/'.$this->id.'-'.$this->link_rewrite;
+        if($absolute)
+            return env('APP_URL').$link;
+        else
+            return $link;
+    }
 }
